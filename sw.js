f52539b8e6b2090ab976a334a402136c8129e900
Alex Gallardo <gallardoalex069@gmail.com>
2020-04-07 12:53:06 -0600

    modificaciones de iphone x+ y cache dinamico

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('js/sw-utils.js')
 
 const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 // APP SHELL, todo lo necesario para que la aplicacion funcione
@@ -40,6 +40,9 @@ self.addEventListener('activate', event => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key != STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key)
+            }
+            if (key != DYMANIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key)
             }
         })
